21960cb1b8d7a73d3051c4fe876aafda1ee09299
Jens Stein Jørgensen <jsj@dbc.dk>
2017-09-28 10:01:29 +0200

    Add component for flow binder editing

---
+++ src/components/FlowBinderEdit.js
@@ -0,0 +1,104 @@
+import React from "react";
+
+import RecordSplitterConstants from "../model/RecordSplitterConstants";
+
+class BaseSelect extends React.Component {
+	render() {
+		return (
+			<div className="form-group">
+				<label htmlFor={this.props.name}>{this.props.value}</label>
+				<select name={this.props.name}>
+					{this.props.children}
+				</select>
+			</div>
+		)
+	}
+}
+
+class FlowSelect extends React.Component {
+	render() {
+		return (
+			<BaseSelect name="flow" value="flow"/>
+		)
+	}
+}
+
+class SinkSelect extends React.Component {
+	render() {
+		return (
+			<BaseSelect name="sink" value="sink"/>
+		)
+	}
+}
+
+class SubmitterSelect extends React.Component {
+	render() {
+		return (
+			<div className="form-group">
+				<label htmlFor="submitters">submitters</label>
+				<select multiple>
+				</select>
+			</div>
+		)
+	}
+}
+
+class RecordSplitterSelect extends React.Component {
+	render() {
+		return (
+			<BaseSelect name="record-splitter" value="record splitter">
+			{
+				Object.keys(RecordSplitterConstants).map((key, i) => <option key={i} value={RecordSplitterConstants[key]}>{RecordSplitterConstants[key]}</option>)
+			}
+			</BaseSelect>
+		)
+	}
+}
+
+class FlowBinderEdit extends React.Component {
+	render() {
+		return (
+			<form>
+				<div className="form-group">
+					<label htmlFor="name:">name</label>
+					<input type="text" name="name"/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="description:">description</label>
+					<input type="text" name="description"/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="format:">format</label>
+					<input type="text" name="format"/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="packaging:">packaging</label>
+					<input type="text" name="packaging"/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="charset:">charset</label>
+					<input type="text" name="charset"/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="destination:">destination</label>
+					<input type="text" name="destination"/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="priority:">priority</label>
+					<select name="priority">
+						<option value="1">low</option>
+						<option value="2">mid</option>
+						<option value="3">high</option>
+					</select>
+				</div>
+				<RecordSplitterSelect/>
+				<SubmitterSelect/>
+				<FlowSelect/>
+				<SinkSelect/>
+				<input type="submit" value="save"/>
+			</form>
+		)
+	}
+}
+
+export default FlowBinderEdit;
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import {Route, Switch} from "react-router-dom";
 
 import FlowsList from "./FlowsList";
+import FlowBinderEdit from "./FlowBinderEdit";
 import FlowBindersList from "./FlowBindersList";
 import JobInfo from "./JobInfo";
 import JobList from "./JobList";
@@ -13,6 +14,7 @@ const Main = () => (
 			<Route exact path="/" component={JobList}/>
 			<Route exact path="/jobinfo/:jobId" component={JobInfo}/>
 			<Route exact path="/flowbinders" component={FlowBindersList}/>
+			<Route exact path="/flowbinders/:flowBinderId/edit" component={FlowBinderEdit}/>
 			<Route exact path="/flows" component={FlowsList}/>
 			<Route path="*" component={NotFound}/>
 		</Switch>
