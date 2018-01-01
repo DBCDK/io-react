2e90a01c52b4cef2f0ba034ecdac88e2cdba1a8e
Jens Stein Jørgensen <jsj@dbc.dk>
2018-01-01 16:00:50 +0100

    Populate flow binder edit component with content

--- src/components/FlowBinderEdit.js
+++ src/components/FlowBinderEdit.js
@@ -1,5 +1,7 @@
 import React from "react";
+import PropTypes from "prop-types";
 
+import FlowBinder from "../model/FlowBinder";
 import RecordSplitterConstants from "../model/RecordSplitterConstants";
 import SubmittersView from "./SubmittersView";
 
@@ -8,7 +10,7 @@ class BaseSelect extends React.Component {
 		return (
 			<div className="form-group">
 				<label htmlFor={this.props.name}>{this.props.value}</label>
-				<select name={this.props.name}>
+				<select name={this.props.name} defaultValue={this.props.selected}>
 					{this.props.children}
 				</select>
 			</div>
@@ -35,7 +37,7 @@ class SinkSelect extends React.Component {
 class RecordSplitterSelect extends React.Component {
 	render() {
 		return (
-			<BaseSelect name="record-splitter" value="record splitter">
+			<BaseSelect name="record-splitter" value="record splitter" selected={this.props.recordSplitter}>
 			{
 				Object.keys(RecordSplitterConstants).map((key, i) => <option key={i} value={RecordSplitterConstants[key]}>{RecordSplitterConstants[key]}</option>)
 			}
@@ -44,33 +46,48 @@ class RecordSplitterSelect extends React.Component {
 	}
 }
 
+RecordSplitterSelect.propTypes = {
+	recordSplitter: PropTypes.string
+};
+
+RecordSplitterSelect.defaultProps = {
+	recordSplitter: ""
+};
+
 class FlowBinderEdit extends React.Component {
 	render() {
+		const id = Number.parseInt(this.props.match.params.flowBinderId);
+		let flowBinder = this.context.store.getState().flowBinders.find(
+			f => f.id === id);
+		if(flowBinder === undefined) {
+			flowBinder = new FlowBinder();
+			flowBinder.content = {};
+		}
 		return (
 			<form>
 				<div className="form-group">
 					<label htmlFor="name:">name</label>
-					<input type="text" name="name"/>
+					<input type="text" name="name" defaultValue={flowBinder.content.name}/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="description:">description</label>
-					<input type="text" name="description"/>
+					<input type="text" name="description" defaultValue={flowBinder.content.description}/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="format:">format</label>
-					<input type="text" name="format"/>
+					<input type="text" name="format" defaultValue={flowBinder.content.format}/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="packaging:">packaging</label>
-					<input type="text" name="packaging"/>
+					<input type="text" name="packaging" defaultValue={flowBinder.content.packaging}/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="charset:">charset</label>
-					<input type="text" name="charset"/>
+					<input type="text" name="charset" defaultValue={flowBinder.content.charset}/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="destination:">destination</label>
-					<input type="text" name="destination"/>
+					<input type="text" name="destination" defaultValue={flowBinder.content.destination}/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="priority:">priority</label>
@@ -80,8 +97,8 @@ class FlowBinderEdit extends React.Component {
 						<option value="3">high</option>
 					</select>
 				</div>
-				<RecordSplitterSelect/>
-				<SubmittersView withLabel={true}/>
+				<RecordSplitterSelect recordSplitter={flowBinder.content.recordSplitter}/>
+				<SubmittersView withLabel={true} flowBinderId={flowBinder.id}/>
 				<FlowSelect/>
 				<SinkSelect/>
 				<input type="submit" value="save"/>
@@ -89,5 +106,9 @@ class FlowBinderEdit extends React.Component {
 		)
 	}
 }
+
+FlowBinderEdit.contextTypes = {
+	store: PropTypes.object.isRequired
+};
 
 export default FlowBinderEdit;
